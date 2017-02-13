3fc5221c228aa1c9f1208a752274c008a119d3bc
koskine6 <esa.t.koskinen@aalto.fi>
2017-02-13 20:07:55 +0200

    Initial commit; made some test files for remote compiling.

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hi! Let's make a new object!");
+		Hello2 test = new Hello2("Test message.");
+		System.out.println(test);
+	}
+}
---
+++ Hello2.java
@@ -0,0 +1,10 @@
+public class Hello2 {
+	String message;
+	public Hello2(String in) {
+		this.message = in;
+	}
+
+	public String toString() {
+		return "Hi, my message is: " + this.message;
+	}
+}
