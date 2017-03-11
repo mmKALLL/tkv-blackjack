c1ff67f2fd6999beb86c9616709cce414bde604a
koskine6 <esa.t.koskinen@aalto.fi>
2017-03-11 19:29:28 +0200

    Added structure into client-side NetworkManager.

---
+++ src/tkv_project/client/NetworkManager.java
@@ -0,0 +1,38 @@
+package tkv_project.client;
+
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+    NetworkManager handles the connectivity of a single client to the game server,
+    and had methods for exchanging messages over the network.
+ */
+class NetworkManager extends Thread {
+    
+    private Socket serverConnection;
+    private BufferedReader in;
+    
+    public NetworkManager(SocketAddress sockaddr) {
+        /* initialize socket into ipaddr, in port x */
+        serverConnection = new Socket(sockaddr.getAddress(), sockaddr.getPort());
+        in = new BufferedReader(new InputStreamReader(serverConnection.getInputStream()));
+    }
+    
+    private receiveMessage() {
+        // TODO
+    }
+    
+    void sendMessage(String msg) {
+        /* send msg over the socket */
+        // TODO
+    }
+    
+    public void run() {
+        // Creating class should call NetworkManagerInstance.start() to execute this in a new thread.
+        // Wait for messages, etc...
+        
+    }
+    
+}
